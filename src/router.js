83385631724264175ed56286bc07d37d5bef7c74
Your Father <1526702614@qq.com>
2019-09-18 20:28:32 +0800

    页面分析新建登陆和主页

--- src/router.js
+++ src/router.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/home/' // 路由级组件
-
+import Login from './views/login/'
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     path: '/',
-    name: 'home',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     component: Home
+  },
+  {
+    path: '/login',
+    component: Login
   }
     // {
     //   path: '/about',
