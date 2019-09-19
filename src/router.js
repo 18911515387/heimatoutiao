fa1e17cd3c51d21b197bcff0a70ea521e7ef41a3
Your Father <1526702614@qq.com>
2019-09-19 15:05:31 +0800

    login页面图片完成，表单验证完成到手机正则表达式

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/home/' // 路由级组件
-import Login from './views/login/'
+import Login from './views/login'
 Vue.use(Router)
 
 export default new Router({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,74 @@
 <template>
-  <div>我是登陆页</div>
+  <div class="login">
+    <el-card class="login-card">
+      <div class="title">
+        <img src="../../assets/imgs/logo_index.png">
+      </div>
+      <el-form ref="" :model="ruleBase" :rules="rules" style="margin-top:15px">
+        <el-form-item prop="phone">
+          <el-input v-model="ruleBase.phone" placeholder='请输入手机号'></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="ruleBase.code" class="code-i" placeholder='请输入验证码'></el-input>
+          <el-button type="primary" style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop="protocol">
+          <el-checkbox v-model="ruleBase.protocol">
+            <span class="protocol">我已阅读并同意<a href="#">用户协议</a>和<a href="#">隐私条款</a> </span>
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" round style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      ruleBase: {
+        phone: '',
+        code: '',
+        protocol: false
+      },
+      rules: {
+        phone: [
+          { require: true, message: '请输入手机号' },
+          { pattern: /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/, messagae: '请输入正确的手机号' }
+        ]
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .login{
+    background-image: url('../../assets/imgs/95.jpg');
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card {
+      height: 400px;
+      width: 500px;
+      .title{
+        text-align: center;
+        height: 50px;
+      }
+      .protocol a {
+        color: skyblue;
+      }
+      .code-i {
+        width:60%;
+      }
+      .el-item {
+        margin-bottom: 15px;
+      }
+    }
+  }
 </style>
