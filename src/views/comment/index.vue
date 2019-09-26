481fb00d7ae4450b733282083492731748138f3b
Your Father <1526702614@qq.com>
2019-09-26 20:45:42 +0800

    打开或者关闭评论的axios

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -7,10 +7,19 @@
     </bread-crumb>
     <el-table :data="list">
       <el-table-column width='650px' label="标题" prop="title"></el-table-column>
-      <el-table-column align="center" label="评论状态" prop="comment_status"></el-table-column>
+      <el-table-column :formatter="formatter" align="center" label="评论状态" prop="comment_status"></el-table-column>
       <el-table-column align="center" label="总评论数" prop="total_comment_count"></el-table-column>
       <el-table-column align="center" label="粉丝评论数" prop="fans_comment_count"></el-table-column>
-      <el-table-column align="center" label="操作" prop="address"></el-table-column>
+      <el-table-column align="center" label="操作" prop="address">
+        <template slot-scope="obj">
+          <el-button type="text">修改</el-button>
+          <el-button type="text" @click="closPoen(obj.row)">
+          {{
+            obj.row.comment_status? "关闭评论" : "打开评论"
+          }}
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -30,6 +39,21 @@ export default {
         params: { response_type: 'comment' }
       }).then(result => {
         this.list = result.data.results
+      }
+      )
+    },
+    formatter (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    },
+    closPoen (row) {
+      let mess = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`你是否${mess}评论`, {}).then(() => {
+        this.$axios({
+          url: 'comments/status',
+          method: 'put',
+          params: {},
+          body: {}
+        })
       }
       )
     }
